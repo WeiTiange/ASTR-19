4f89109deb1cd2e58eaa0a70d91b6c845b897336
TIANGE WEI <twei14@ucsc.edu>
2022-04-03 14:47:27 -0700

    Create Session_1_Prompt.py

---
+++ Code Journal #1/Session_1_Prompt.py
@@ -0,0 +1,12 @@
+def Introduction():
+    print("\n\nFull Name: Tiange Wei\n")
+    print("I don't really have a favorite movie. I kind of like every movie I watched from the beginning to the end, because if I don't like a movie I wouldn't finish it.\n")
+    print("My favorite food is strawberry.\n\n")
+
+def main():
+    Introduction()
+
+
+if __name__ == '__main__':
+    main()
+    
