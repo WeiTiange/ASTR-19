4a2f08537e8a2955391aff13469d05d5dc369572
TIANGE WEI <twei14@ucsc.edu>
2022-04-22 11:05:09 -0700

    Session1 prompt 5 edit, promt6 add

--- Code_Journal_1/Session_5_Prompt.py
+++ Code_Journal_1/Session_5_Prompt.py
@@ -1,5 +1,4 @@
 # Write a Python program that writes out a table of the function sin(x) vs. x, where x is tabulated between 0 and 2 with a thousand entries. Follow the basic Python program structure, including a main program function.
-from tabulate import tabulate
 import numpy as np
 
 
@@ -7,9 +6,9 @@ def main():
     x = np.linspace(0, 2 * np.pi, 1000)
     sinX = np.sin(x)
 
-    result = {x[i]: sinX[i] for i in range(len(x))}
-
-    print(tabulate(result.items(), headers=["x", "sin(x)"]))
+    print(f"{'X' : <30}{'Sin(X)'}")
+    for i in range(len(x)):
+        print(f"{x[i] : <30}{sinX[i]}")
 
 
 if __name__ == '__main__':
