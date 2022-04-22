bed9bf3e63247aa50535b97e52d10c9f974f082f
TIANGE WEI <twei14@ucsc.edu>
2022-04-22 13:35:29 -0700

    Add in-class demos

--- In-class-demo/test.py
+++ In-class-demo/test.py
@@ -1,6 +1,13 @@
 import numpy as np
 
+from Code_Journal_1.Session_5_Prompt import main
+
 x = 1.0
 y = 2.0
 
 print(f"np.log10({x}) = {np.log10(x)}")
+
+
+if __name__ == '__main__':
+    main()
+    
